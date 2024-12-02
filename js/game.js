07895795a597df33251c089be3ec1eac4947932d
Peter-0102 <culp040109@gs.utm.mx>
2024-12-02 07:42:46 -0600

    Juego finalizado

--- js/game.js
+++ js/game.js
@@ -1,14 +1,41 @@
 // Variables blobales
 
-let timer_time = 30;
+let timer_time = 1500;
 let timer__object
 let palabra_global = "";
 
 let contador_veces_ganadas = 0;
 let nombre_player = "";
 
+let num_pistas = 3;
 
+const gameSection = document.getElementById('game-section');
+const nextButton = document.getElementById('next-button');
+const menu_de_ingreso = document.getElementById('menu-de-ingreso');
 const input_user= document.getElementById("nombre_user");
+const menu = document.getElementById('menu');
+
+
+
+const playButton = document.getElementById('play-button');
+
+
+//Sonidos
+
+const clickSound = './sounds/sfx_button.mp3'
+const wrong_Sound = './sounds/wrong_sound.wav'
+const correct_Sound = './sounds/correct_sound.wav'
+const win_Sound = './sounds/success.wav'
+const lose_sound = './sounds/lose_sound.wav'
+
+document.addEventListener('DOMContentLoaded', () => {
+    menu.style.display = 'block';
+    menu_de_ingreso.style.display = 'none';
+    gameSection.style.display = 'none';
+  });
+
+
+
 
 input_user.addEventListener('keypress',function(event){
     if (event.key === 'Enter') {
@@ -18,90 +45,80 @@ input_user.addEventListener('keypress',function(event){
     }
 });
 
-
-const playButton = document.getElementById('play-button');
-const menu = document.getElementById('menu');
-const loginsection = document.getElementById('menu-de-ingreso');
-
-  playButton.addEventListener('click', () => {
-    // Añadir clase para la animación de desvanecimiento
+playButton.addEventListener('click', () => {
+    playSound(clickSound);
     menu.classList.add('fade-out');
-    // Mostrar el juego después de la animación (1s)
     setTimeout(() => {
-      menu.style.display = 'none'; // Ocultar el menú completamente
-
+      menu.style.display = 'none'; 
+      menu_de_ingreso.style.display = 'block';
+      menu_de_ingreso.classList.add('fade-in');
+    }, 1000); 
+  });
+  
+  // Evento para "Continuar"
+  nextButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    playSound(clickSound);
+    if (input_user.value.trim() === "") {
+    //   alert("Ingrese un usuario válido.");
+      return;
+    }
+  
+    menu_de_ingreso.classList.add('fade-out');
+    setTimeout(() => {
+      menu_de_ingreso.style.display = 'none'; 
+      gameSection.style.display = 'block'; 
+      gameSection.classList.add('fade-in');
+  
+      user_data(); 
+      timer_to_down(); 
+      recibePalabra(); 
     }, 1000);
   });
-
-const gameSection = document.getElementById('game-section');
-const nextButton = document.getElementById('next-button');
-const menu_de_ingreso = document.getElementById('menu-de-ingreso');
-
-nextButton.addEventListener('click', ()=>
-{
-    if(input_user.value)
-    {
-        menu_de_ingreso.classList.add('fade-out');
-        gameSection.classList.add('fade-int');
-    
-        setTimeout(()=>
-        {
-            user_data()
-            gameSection.style.display = 'block'; // Mostrar el juego
-            recibePalabra();
-        },500);
-    
-    }else{
-        alert("Ingrese un usuario");
-    }
-});
-
-
-
-
-
 
 
 // Gpas
 function fill_gabps(n,palabra) {
     let form_to_add_gaps = document.getElementById("gaps");
     const array_Inputs  = []
-    // Usar un bucle para agregar los campos de entrada de texto
+    
     for (let i = 0; i < n; i++) {
-      // Crear el elemento input
+      
       let input = document.createElement("input");
-      input.className = "inputs-letras"; // Asignar la clase
-      input.type = "text"; // Tipo de campo
-      input.maxLength = 1; // Limitar a un solo carácter
-      input.pattern = "[A-Za-z]+"; // Solo permitir letras
-      input.id = "input-texto-" + i; // Asignar un id único
+      input.className = "inputs-letras"; 
+      input.type = "text";
+      input.maxLength = 1; 
+      input.pattern = "[A-Za-záéíóúÁÉÍÓÚñÑ]+";
+      input.id = "input-texto-" + i; 
       
       palabra_global +=""+i;
       input.addEventListener('input', function (event) {
-        // Reemplazar cualquier cosa que no sea una letra
-        this.value = this.value.replace(/[^A-Za-z]/g, '');
         
-        
+        this.value = this.value.replace(/[^A-Za-záéíóúÁÉÍÓÚñÑ]/g, '');
+
         if(validar_letra(this,palabra,i))
         {
             palabra_global=palabra_global.replace(""+i,palabra[i])
-            console.log("Estamos añaniendo una letra", palabra_global)
-
+            playSound(correct_Sound);
+            // console.log("Estamos añaniendo una letra", palabra_global)
+            this.classList.add('respuesta_correcta');
             if (palabra_global == palabra)
             {
-                contador_veces_ganadas++;
-                alert("Haz ganado");
-                
-                
+                playAudio(false);
+                playSound(win_Sound);
+                playAudio(true);
+                contador_veces_ganadas++;              
                 showVictoryModal();
             }
+        }else{
+            playSound(wrong_Sound);
         }
       });
 
 
 
       array_Inputs.push(input);
-      // Agregar el input al formulario
+      
       form_to_add_gaps.appendChild(input);
     }
 
@@ -121,7 +138,7 @@ const recibePalabra = async () =>{
         if(response.ok)
         {
             const jsonResponse = await response.json();
-
+            
             logic_Game(jsonResponse[0])
         }
     }catch(error){
@@ -130,16 +147,19 @@ const recibePalabra = async () =>{
     }
 
 }
-
+let palabra_correcta 
+let arreglo_de_inputs_blogal 
 function logic_Game(palabra) {
     // Obtener palabra
-    
+    palabra_correcta = palabra;
     console.log(palabra)
     const let_Word = palabra.length;
 
-    //aregkis de inputs :D
-    const arreglo_de_inputs = fill_gabps(let_Word,palabra);  
-    timer_to_down();
+    //areglos de inputs :D
+    const arreglo_de_inputs = fill_gabps(let_Word,palabra); 
+    arreglo_de_inputs_blogal = arreglo_de_inputs;
+    
+    
 
 }
 
@@ -194,34 +214,45 @@ function mover_section(id){
 }
 
 function showVictoryModal() {
-    // Usa Bootstrap para mostrar el modal
-    const victoryModal = new bootstrap.Modal(document.getElementById('victoryModal'));
+    
+    const victoryModal = new bootstrap.Modal(document.getElementById('victoryModal'),
+    {
+        backdrop: 'static',  
+        keyboard: false  
+    });
     
     stopTimer()
     victoryModal.show();
   }
   
   function showLoseModal() {
-    // Usa Bootstrap para mostrar el modal
-    const victoryModal = new bootstrap.Modal(document.getElementById('loseModal'));
+    playAudio(false);
+    playSound(lose_sound);
+    playAudio(true);
+    const victoryModal = new bootstrap.Modal(document.getElementById('loseModal'),{
+        backdrop: 'static',  
+        keyboard: false   
+    });
     
     stopTimer()
     victoryModal.show();
   }
   const replayButton = document.getElementById('replayButton');
   replayButton.addEventListener('click', () => {
-    // Reinicia el juego
+    
+    playSound(clickSound);
     resetGame();
-    // Cierra el modal
+    
     const victoryModal = bootstrap.Modal.getInstance(document.getElementById('victoryModal'));
     victoryModal.hide();
   });
 
   const replayButton2 = document.getElementById('replayButton2');
   replayButton2.addEventListener('click', () => {
-    // Reinicia el juego
+    
+    playSound(clickSound);
     resetGame();
-    // Cierra el modal
+    
     const loseModal = bootstrap.Modal.getInstance(document.getElementById('loseModal'));
     loseModal.hide();
   });
@@ -229,51 +260,59 @@ function showVictoryModal() {
   
 
   function resetGame() {
-    timer_time = 30;
     stopTimer()
-    palabra_global = ""; // Reinicia la palabra global
-    document.getElementById('gaps').innerHTML = ""; // Limpia los inputs del juego
-    recibePalabra(); // Genera una nueva palabra
+    timer_time = 15;
+    num_pistas=3;
+    timer_to_down();
+    reset_Dibujo();
+    pistas_button.textContent = 'Pistas ' + num_pistas;
+    palabra_global = ""; 
+    document.getElementById('gaps').innerHTML = "";
+    recibePalabra(); 
   }
   
 
-  // Obtener referencia al botón "Cerrar" del modal
+  
 const closeButton = document.getElementById('closeButton');
 
-// Escuchar el clic en el botón "Cerrar"
+
 closeButton.addEventListener('click', () => {
-  guardarYRedirigir();
+    playSound(clickSound);
+    setTimeout(()=>{
+        guardarYRedirigir();
+    },1000);
 });
 
 const closeButton2 = document.getElementById('closeButton2');
 
-// Escuchar el clic en el botón "Cerrar"
+
 closeButton2.addEventListener('click', () => {
+    playSound(clickSound);
   guardarYRedirigir();
 });
 
 function guardarYRedirigir() {
-  // Verificar si hay un nombre de usuario válido
+ 
   if (!nombre_player) {
     alert("Error: El nombre de usuario no es válido.");
     return;
   }
 
-  // Recuperar los puntajes almacenados
+  
   let savedScores = localStorage.getItem("gameScores");
   if (!savedScores) {
-    savedScores = {}; // Inicializar si no existen puntajes
+    savedScores = {}; 
   } else {
-    savedScores = JSON.parse(savedScores); // Convertir de JSON a objeto
+    savedScores = JSON.parse(savedScores); 
   }
 
-  // Verificar y actualizar puntaje solo si es mayor al actual
+  
   if (!savedScores[nombre_player] || savedScores[nombre_player] < contador_veces_ganadas) {
-    savedScores[nombre_player] = contador_veces_ganadas; // Actualizar puntaje
-    localStorage.setItem("gameScores", JSON.stringify(savedScores)); // Guardar en localStorage
+    savedScores[nombre_player] = contador_veces_ganadas; 
+    localStorage.setItem("gameScores", JSON.stringify(savedScores)); 
   }
 
-  // Redirigir a la página de puntuaciones
+  
   window.location.href = "score.html";
 }
 
@@ -283,22 +322,111 @@ function timer_to_down() {
 
     let contador_element = document.getElementById("contador"); 
     timer__object = setInterval(() => {
-        console.log("Contador: " + timer_time); // Mostrar el contador
+        console.log("Contador: " + timer_time); 
         contador_element.innerText= "";
         contador_element.innerText= timer_time;
-        timer_time--; // Decrementar cada segundo
+        timer_time--; 
 
         if (timer_time < 0) {
-            clearInterval(timer2); // Detener el temporizador cuando llegue a 0
-
+            clearInterval(timer__object); 
+            
             console.log("El tiempo se ha agotado");
             showLoseModal();
         }
-    }, 1000); // Ejecutar cada segundo
+    }, 1000); 
 }
 
 
 function stopTimer() {
-    clearInterval(timer__object);  // Detener el temporizador desde fuera de la función
+    clearInterval(timer__object);  
     console.log("Temporizador detenido");
+}
+
+const pistas_button = document.getElementById("pistas");
+
+pistas_button.addEventListener('click',()=>
+{
+    generador_pistas()
+});
+
+function generador_pistas()
+{
+    if(num_pistas>0)
+    {
+    
+        let tamaño_palabra = palabra_global.length;
+
+        const numero_pista = Math.floor(Math.random()*tamaño_palabra )+1;
+        console.log("Numero de letra "+ num_pistas + "se cambiara ",palabra_global[numero_pista]);
+        arreglo_de_inputs_blogal[numero_pista].value = palabra_correcta[numero_pista]; 
+        arreglo_de_inputs_blogal[numero_pista].classList.add("respuesta_correcta");
+        palabra_global=palabra_global.replace(""+numero_pista,palabra_correcta[numero_pista])
+        
+        num_pistas--;
+        pistas_button.textContent = 'Pistas ' + num_pistas;
+    }else{
+        // alert("pistas agotadas")
+       
+    }
+}
+
+let erroresJugador = -1;
+
+const intentosMaximos = 8;
+
+
+const ahorcadoImg = document.getElementById('ahorcado-img');
+
+function reset_Dibujo()
+{
+    ahorcadoImg.src = `./img/Personaje/male/ahorcado.png`;
+    erroresJugador=-1;
+}
+
+function actualizarAhorcado() {
+    if (erroresJugador < intentosMaximos) {
+      ahorcadoImg.src = `./img/Personaje/male/ahorcado${erroresJugador}.png`;
+      console.log(erroresJugador)
+    } else {
+        reset_Dibujo();
+      
+      showLoseModal();
+    }
+}
+function registrarError() {
+    erroresJugador++;
+    actualizarAhorcado();
+}
+
+function validar_letra(input, palabra, numero_de_input) {
+    if (palabra[numero_de_input] === input.value) {
+      return true;
+    } else {
+      registrarError();
+      return false;
+    }
+  }
+
+
+
+
+
+function playSound(soundFile)
+{
+    const audio = new Audio(soundFile)
+    audio.play();
+}
+  
+
+
+function playAudio(play)
+{
+    if(play)
+    {
+        console.log("play musica");
+        document.getElementById("audio").play();
+    }else{
+        console.log("Pause musica");
+        document.getElementById("audio").pause();
+    }
 }
